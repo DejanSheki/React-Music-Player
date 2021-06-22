bb6f837d3223ef8321400b719007a7d41fdea5f7
Dejan <dejan.sheki.lukic@gmail.com>
2021-06-22 19:36:18 +0200

    Added Fontawesom some...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './styles/app.scss';
+import Player from './components/Player';
+import Song from './components/Song';
+import data from './util';
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
